143599f4468184db1cda98f0f8ec7f8891969861
Aakysank <aakashjs1993@gmail.com>
2024-06-16 13:02:27 +0530

    Sorting algorithms

---
+++ sorting.h
@@ -0,0 +1,140 @@
+#pragma once
+void BubbleSort(int A[], int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int flag = 0;
+		for (int j = 0; j < size - 1 - i; j++)
+		{
+			if (A[j] > A[j + 1])
+			{
+				int temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = A[j];
+				flag = 1;
+			}
+		}
+
+		if (flag == 0)//flag to break if the array is already sorted -- if the array is sorted, the swapping wont happen.
+			break;
+	}
+}
+void InsertionSort(int A[], int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int j = i - 1;
+		int x = A[i];
+
+		while (j > -1 && A[j] > x)
+		{
+			A[j+1] = A[j];
+			j--;
+		}
+
+		A[j+1] = x;
+	}
+}
+
+void SelectionSort(int A[], int n)
+{
+	// index is selected for which you need to select the element appropriate element
+	//i must point to the current index, k and j are incrementers, if (jth element is less than k, move k to jth position and increment j till end)
+	//swap kth position with current i
+	for (int i = 0; i < n-1; i++)
+	{
+		int k = i, j = i + 1;
+		for (; j < n; j++)
+		{
+			if (A[k] > A[j])
+			{
+				k = j;
+			}
+		}
+
+		int temp = A[k];
+		A[k] = A[i];
+		A[i] = temp;
+	}
+}
+
+//Quick sort
+int Partition(int A[], int l, int h)
+{
+	int pivot = A[l];
+	int i = l, j = h;
+	do
+	{
+		do { i++; } while (A[i] <= pivot);
+		do { j--; } while (A[j] > pivot);
+
+		if (i < j)
+		{
+			int temp = A[i];
+			A[i] = A[j];
+			A[j] = temp;
+		}
+	} while (i < j);
+
+	int temp = A[l];
+	A[l] = A[j];
+	A[j] = temp;
+	return j;
+}
+
+void QuickSort(int A[], int l, int h)
+{
+	if (l < h)
+	{
+		int j = Partition(A, l, h);
+		QuickSort(A, l, j);
+		QuickSort(A, j+1, h);
+	}
+
+	return;
+}
+void Merge(int A[], int l, int mid, int h)
+{
+	int i = l, j = mid+1, k = l;
+
+	int B[100] = { 0 };
+	while (i <= mid && j <= h)
+	{
+		if (A[i] < A[j])
+			B[k++] = A[i++];
+		else
+			B[k++] = A[j++];
+	}
+
+	while (i <= mid) {
+		B[k++] = A[i++];
+	}
+	while (j <= h) {
+		B[k++] = A[j++];
+	}
+
+
+	for (int i = l; i <= h; i++) {
+		A[i] = B[i];
+	}
+}
+
+void IMergeSort(int A[], int n)
+{
+	int p = 0, l = 0, h = 0, mid = 0, i = 0;
+
+	for (p = 2; p <= n; p = p * 2)
+	{
+		for (i = 0; i + p - 1 < n; i = i + p)
+		{
+			l = i;
+			h = i + p - 1;
+			mid = (l + h) / 2;
+
+			Merge(A, l, mid, h);
+		}
+	}
+
+	if (p / 2 < n)
+		Merge(A, 0, (p / 2)-1, n);
+}
